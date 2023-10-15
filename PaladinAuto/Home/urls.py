29a543a5aca1988184a8ebd974c2d6be87974a71
-tEmpOoo <efimaniskin@gmail.com>
2023-10-16 01:11:41 +0300

    Second commit
    Add some buttons and logo

--- PaladinAuto/Home/urls.py
+++ PaladinAuto/Home/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('application/', views.submit_application, name='application'),
+    path('work-process/', views.work_process, name='work_process'),
 ]
--- PaladinAuto/Home/views.py
+++ PaladinAuto/Home/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
 import time
-now_time = {'now_time':time.time()}
+
+menu = [
+    {'title':'Главная', 'name':'home'},
+    {'title':'Рабочий процесс', 'name':'work_process'},
+    {'title':'Отзывы', 'name':'reviews'},
+    {'title':'Контакты', 'name':'contacts'},
+    {'title':'Оставить заявку', 'name':'application'},
+]
 def home(request):
-    return render(request,'Home/home_page.html',now_time)
+    data = {
+        'title':'Главная страница',
+        'menu':menu,
+    }
+    return render(request,'Home/home_page.html', context=data)
+
+def contacts(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+    }
+    return render(request,'Home/contacts.html', context=data)
+
+def reviews(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+    }
+    return render(request,'Home/reviews.html', context=data)
+
+def submit_application(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+    }
+    return render(request,'Home/submit_application.html', context=data)
+
+def work_process(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+    }
+    return render(request,'Home/work_process.html', context=data)
+
