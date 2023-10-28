5423118e29227d6bd6ef96c2ad315760bbd08760
-tEmpOoo <efimaniskin@gmail.com>
2023-10-28 15:11:07 +0300

    commit 3
    update requirements.py add some functions

--- PaladinAuto/Home/models.py
+++ PaladinAuto/Home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Applications(models.Model):
     name = models.CharField(max_length=100)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=11)
     email = models.EmailField(max_length=100, blank=True)
     comment = models.TextField(max_length=500, blank=True)
     create_time = models.DateTimeField(auto_now=True)
