d8124595fc0560557cc3defde5f5733d1ee6935e
-tEmpOoo <efimaniskin@gmail.com>
2023-11-05 23:38:16 +0300

    Fifth commit
    Create a status for application

--- PaladinAuto/Home/models.py
+++ PaladinAuto/Home/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Applications(models.Model):
-    class Status(models.IntegerChoices):
-        AWAITING = 0, "В ожидании"
-        COMPLETED = 1, "Выполнено"
-        # CANCELED = 'Отменено'
+    class Status(models.TextChoices):
+        AWAITING = "В ожидании","В ожидании"
+        COMPLETED = "Выполнено","Выполнено"
+        CANCELED = 'Отменено','Отменено'
 
     name = models.CharField(max_length=100, verbose_name='Имя клиента')
     phone = models.CharField(max_length=11, verbose_name='Телефон')
     email = models.EmailField(max_length=100, blank=True, verbose_name='Почта')
     comment = models.TextField(max_length=500, blank=True, verbose_name='Комментарий')
     create_time = models.DateTimeField(auto_now=True, verbose_name='Дата создания заявки')
-    status = models.BooleanField(choices=Status.choices, default=Status.AWAITING, blank=True)
+    status = models.TextField(choices=Status.choices, default=Status.AWAITING, blank=True, verbose_name='Статус')
 
     class Meta:
         verbose_name = 'Заявка'
